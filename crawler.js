8aecae271c3a87e1c5e0df1871e929316e377ea3
bamby715 <tafayuaghsjgwhf@gmail.com>
2026-03-17 23:07:32 +0800

    Update crawler.js

--- crawler.js
+++ crawler.js
@@ -3,7 +3,10 @@ const fs = require('fs');
 
 (async () => {
   console.log('启动浏览器...');
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ 
+  headless: true,
+  args: ['--no-sandbox', '--disable-setuid-sandbox']
+});
   const page = await browser.newPage();
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36');
 
